a2fdab765ed371b5f4d73fa46b28cba4be579b13
jneilliii <jneilliii@gmail.com>
2015-06-19 04:34:14 +0000

    Initial Upload

---
+++ octoprint_stlviewer/__init__.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class stlviewer(octoprint.plugin.StartupPlugin,
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin,
+                       octoprint.plugin.AssetPlugin):
+					   
+	def on_after_startup(self):
+		self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+
+	def get_settings_defaults(self):
+		return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+
+	def get_template_configs(self):
+		return [
+		]
+
+	def get_assets(self):
+		return dict(
+			js=["js/stlviewer.js"],
+			css=["css/stlviewer.css"],
+			less=["less/stlviewer.less"]
+		)
+
+__plugin_name__ = "OctoPrint-STLViewer"
+__plugin_implementation__ = stlviewer()
