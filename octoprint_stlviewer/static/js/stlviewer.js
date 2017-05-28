9e933392bc5f4cfa0f756abb069d2583b15a402c
jneilliii <jneilliii+github@gmail.com>
2017-05-28 12:38:08 -0400

    Update stlviewer.js

--- octoprint_stlviewer/static/js/stlviewer.js
+++ octoprint_stlviewer/static/js/stlviewer.js
@@ -98,7 +98,7 @@ $(function () {
 			// This is a list of dependencies to inject into the plugin, the order which you request here is the order
 			// in which the dependencies will be injected into your view model upon instantiation via the parameters
 			// argument
-			["gcodeFilesViewModel"],
+			["filesViewModel"],
 
 			// Finally, this is the list of all elements we want this view model to be bound to.
 			[("#tab_plugin_stlviewer")]
