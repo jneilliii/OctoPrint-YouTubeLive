0fb568c9d389006429fa5032580246c1ef873a34
jneilliii <jneilliii+github@gmail.com>
2017-07-22 19:40:47 -0400

    Finalizing initial release.

--- octoprint_youtubelive/__init__.py
+++ octoprint_youtubelive/__init__.py
@@ -7,21 +7,51 @@ class youtubelive(octoprint.plugin.StartupPlugin,
 				octoprint.plugin.TemplatePlugin,
 				octoprint.plugin.AssetPlugin,
                 octoprint.plugin.SettingsPlugin):
-					   
+	
+	##~~ StartupPlugin
 	def on_after_startup(self):
 		self._logger.info("OctoPrint-YouTubeLive loaded!")
-
+	
+	##~~ TemplatePlugin
 	def get_template_configs(self):
 		return [dict(type="settings",custom_bindings=True)]
-
+		
+	##~~ AssetPlugin
 	def get_assets(self):
 		return dict(
 			js=["js/youtubelive.js"],
 			css=["css/youtubelive.css"]
 		)
-		
+	
+	##~~ SettingsPlugin
 	def get_settings_defaults(self):
-		return dict(channel_id="")	
+		return dict(channel_id="")
+
+	##~~ Softwareupdate hook
+	def get_update_information(self):
+		return dict(
+			youtubelive=dict(
+				displayName="YouTube Live",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="jneilliii",
+				repo="OctoPrint-YouTubeLive",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/jneilliii/OctoPrint-YouTubeLive/archive/{target_version}.zip"
+			)
+		)
 
 __plugin_name__ = "YouTube Live"
-__plugin_implementation__ = youtubelive()
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = youtubelive()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
