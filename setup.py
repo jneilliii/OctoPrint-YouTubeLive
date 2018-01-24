8e982f1ea893d11e7faa74f0b2e3b7560a01bdf1
jneilliii <jneilliii+github@gmail.com>
2018-01-23 20:20:35 -0500

    specific to user docker image https://blog.alexellis.io/live-stream-with-docker/

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_youtubelive"
 plugin_name = "YouTube Live"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "0.2.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/jneilliii/OctoPrint-YouTubeLive"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["docker==2.7.0"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
