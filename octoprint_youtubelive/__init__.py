36ec0831bea81ed85a86de073d11a0411cc48781
jneilliii <jneilliii+github@gmail.com>
2018-01-30 16:21:16 -0500

    Update __init__.py

--- octoprint_youtubelive/__init__.py
+++ octoprint_youtubelive/__init__.py
@@ -61,6 +61,8 @@ class youtubelive(octoprint.plugin.StartupPlugin,
 					filters.append("vflip")
 				if self._settings.global_get(["webcam" "rotate90"]):
 					filters.append("transpose=2")
+				if len(filters) == 0:
+					filters.append("null")
 				try:
 					self.container = self.client.containers.run("octoprint/youtubelive:latest",command=[self._settings.global_get(["webcam","stream"]),self._settings.get(["stream_id"]),",".join(filters)],detach=True,privileged=True,name="YouTubeLive",auto_remove=True)
 					self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=True))
