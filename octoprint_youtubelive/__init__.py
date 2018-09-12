f3043764506bc6c774c6163aceb7e9d5e018773d
sadegroo <39280315+sadegroo@users.noreply.github.com>
2018-09-12 20:30:14 +0200

    Update __init__.py

--- octoprint_youtubelive/__init__.py
+++ octoprint_youtubelive/__init__.py
@@ -94,7 +94,7 @@ class youtubelive(octoprint.plugin.StartupPlugin,
 			if len(filters) == 0:
 				filters.append("null")
 			try:
-				self.container = self.client.containers.run("octoprint/youtubelive:latest",command=[self._settings.global_get(["webcam","stream"]),self._settings.get(["stream_id"]),",".join(filters)],detach=True,privileged=True,name="YouTubeLive",auto_remove=True)
+				self.container = self.client.containers.run("octoprint/youtubelive:latest",command=[self._settings.global_get(["webcam","stream"]),self._settings.get(["stream_id"]),",".join(filters)],detach=True,privileged=True,name="YouTubeLive",network="host",auto_remove=True)
 				self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=True))
 			except Exception, e:
 				self._plugin_manager.send_plugin_message(self._identifier, dict(error=str(e),status=True,streaming=False))
