b211f949ac89cb804bc5b7fc92ff4b65e03a7109
jneilliii <jneilliii+github@gmail.com>
2021-02-24 00:37:27 -0500

    add ffmpeg command line option, copied directly from previous docker entry.sh

--- octoprint_youtubelive/__init__.py
+++ octoprint_youtubelive/__init__.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+import os
+import subprocess
+
 import octoprint.plugin
 from octoprint.server import user_permission
-import docker
 
 class youtubelive(octoprint.plugin.StartupPlugin,
 				octoprint.plugin.TemplatePlugin,
@@ -13,25 +15,25 @@ class youtubelive(octoprint.plugin.StartupPlugin,
 				octoprint.plugin.EventHandlerPlugin):
 	
 	def __init__(self):
-		self.client = docker.from_env()
+		# self.client = docker.from_env()
 		self.container = None
 	
 	##~~ StartupPlugin
 	
 	def on_after_startup(self):
-		self._logger.info("OctoPrint-YouTubeLive loaded! Checking stream status.")
-		try:
-			self.container = self.client.containers.get('YouTubeLive')
-			self._logger.info("%s is streaming " % self.container.name)
-			self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=True))
-		except Exception as e:
-			self._logger.error(str(e))
-			self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=False))
+		self._logger.info("OctoPrint-YouTubeLive loaded!")
+		# try:
+		# self.container = self.client.containers.get('YouTubeLive')
+		# self._logger.info("%s is streaming " % self.container.name)
+		# self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=True))
+		# except Exception as e:
+		# self._logger.error(str(e))
+		# self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=False))
 	
 	##~~ TemplatePlugin
 	
 	def get_template_configs(self):
-		return [dict(type="settings",custom_bindings=False)]
+		return [dict(type="settings", custom_bindings=False)]
 		
 	##~~ AssetPlugin
 	
@@ -44,12 +46,16 @@ class youtubelive(octoprint.plugin.StartupPlugin,
 	##~~ SettingsPlugin
 	
 	def get_settings_defaults(self):
-		return dict(channel_id="",stream_id="",streaming=False,auto_start=False)
+		return dict(channel_id="",
+					stream_id="",
+					streaming=False,
+					auto_start=False,
+					ffmpeg_cmd_options="-re -f mjpeg -framerate 5 -i http://octopi.local/webcam/?action=stream -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -acodec aac -ab 128k -strict experimental -vcodec h264 -pix_fmt yuv420p -g 10 -vb 700k -framerate 5")
 		
 	##~~ SimpleApiPlugin
 	
 	def get_api_commands(self):
-		return dict(startStream=[],stopStream=[],checkStream=[])
+		return dict(startStream=[], stopStream=[], checkStream=[])
 		
 	def on_api_command(self, command, data):
 		if not user_permission.can():
@@ -77,7 +83,7 @@ class youtubelive(octoprint.plugin.StartupPlugin,
 		if event == "PrintStarted" and self._settings.get(["auto_start"]):
 			self.startStream()
 			
-		if event in ["PrintDone","PrintCancelled"] and self._settings.get(["auto_start"]):
+		if event in ["PrintDone", "PrintCancelled"] and self._settings.get(["auto_start"]):
 			self.stopStream()
 			
 	##-- Utility Functions
@@ -85,31 +91,36 @@ class youtubelive(octoprint.plugin.StartupPlugin,
 	def startStream(self):
 		if not self.container:
 			filters = []
-			if self._settings.global_get(["webcam","flipH"]):
+			if self._settings.global_get(["webcam", "flipH"]):
 				filters.append("hflip")
-			if self._settings.global_get(["webcam","flipV"]):
+			if self._settings.global_get(["webcam", "flipV"]):
 				filters.append("vflip")
-			if self._settings.global_get(["webcam","rotate90"]):
+			if self._settings.global_get(["webcam", "rotate90"]):
 				filters.append("transpose=cclock")
 			if len(filters) == 0:
 				filters.append("null")
 			try:
-				self.container = self.client.containers.run("octoprint/youtubelive:latest",command=[self._settings.global_get(["webcam","stream"]),self._settings.get(["stream_id"]),",".join(filters)],detach=True,privileged=False,devices=["/dev/vchiq"],name="YouTubeLive",auto_remove=True,network_mode="host")
-				self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=True))
+				# self.container = self.client.containers.run("octoprint/youtubelive:latest",command=[self._settings.global_get(["webcam","stream"]),self._settings.get(["stream_id"]),",".join(filters)],detach=True,privileged=False,devices=["/dev/vchiq"],name="YouTubeLive",auto_remove=True,network_mode="host")
+				ffmpeg_cmd = "{} {} -f flv rtmp://a.rtmp.youtube.com/live2/{}".format(self._settings.global_get(["webcam", "ffmpeg"]), self._settings.get(["ffmpeg_cmd_options"]), self._settings.get(["stream_id"]))
+				# replace this with actual subprocess command
+				FNULL = open(os.devnull, 'w')
+				self.container = subprocess.Popen(ffmpeg_cmd.split(' '), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=FNULL)
+				self._logger.info(ffmpeg_cmd)
+				self._plugin_manager.send_plugin_message(self._identifier, dict(status=True, streaming=True))
 			except Exception as e:
-				self._plugin_manager.send_plugin_message(self._identifier, dict(error=str(e),status=True,streaming=False))
+				self._plugin_manager.send_plugin_message(self._identifier, dict(error=str(e), status=True, streaming=False))
 		return
 		
 	def stopStream(self):
 		if self.container:
 			try:
-				self.container.stop()
+				self.container.terminate()
 				self.container = None
-				self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=False))
+				self._plugin_manager.send_plugin_message(self._identifier, dict(status=True, streaming=False))
 			except Exception as e:
-				self._plugin_manager.send_plugin_message(self._identifier, dict(error=str(e),status=True,streaming=False))
+				self._plugin_manager.send_plugin_message(self._identifier, dict(error=str(e), status=True, streaming=False))
 		else:
-			self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=False))
+			self._plugin_manager.send_plugin_message(self._identifier, dict(status=True, streaming=False))
 
 	##~~ Softwareupdate hook
 	def get_update_information(self):
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_youtubelive"
 plugin_name = "YouTube Live"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.5.2"
+plugin_version = "0.6.0rc1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/jneilliii/OctoPrint-YouTubeLive"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["docker==2.7.0"]
+plugin_requires = []
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
